43be39c4afb4c4160ebf7b6511bd5bb6cbb8f102
zainbinabid55 <dev@Devs-MacBook-Pro.local>
2024-02-27 21:19:24 +0500

    Created header component

---
+++ pet-service-frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <HeaderComp />
+</template>
+
+<script>
+
+import HeaderComp from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComp,
+  
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: #e0dfdf;
+  padding: 50px;
+}
+
+</style>
---
+++ pet-service-frontend/src/components/Header.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="header-container">
+    <img src="../assets/logo.png" width="100" />
+
+    <div class="links">
+      <a href="#" class="text-secondary text-decoration-none">Help</a>
+      <a href="#" class="text-default text-decoration-none m-4"
+        >Save and Finish Later</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComp",
+};
+</script>
+
+<style scoped>
+.header-container {
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+  width: 100%;
+  height: 60px;
+  background-color: white;
+  position: fixed;
+  top: 0;
+  z-index: 99;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.links {
+  display: flex;
+  align-items: center;
+}
+
+.links a {
+  margin-left: 15px;
+}
+
+.text-default {
+  color: #00a0e5;
+}
+
+@media (max-width: 768px) {
+  .header-container {
+    flex-direction: column;
+    height: auto;
+    padding: 10px;
+  }
+
+  .links {
+    margin-top: 10px;
+    justify-content: center;
+  }
+
+  .links a {
+    margin: 0 5px;
+  }
+}
+</style>
