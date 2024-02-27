f88547a10b172fbbf3b5bd498ac2b13dadeb59d2
zainbinabid55 <dev@Devs-MacBook-Pro.local>
2024-02-27 21:24:05 +0500

    Service for backend call

---
+++ pet-service-frontend/src/services/SymphonyService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api/pet',
+  withCredentials: false,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default {
+  registerPet(data) {
+    return apiClient.post('/create',data);
+  },
+};
